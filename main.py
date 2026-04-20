0ecc147da41a8e127c8946dc958a80794b30e88a
Abdul Manan Maqsood <helloabdulmanan@gmail.com>
2026-04-20 00:20:07 -0700

    Add CORS middleware for frontend integration

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from app.api.routes import router
 from app.services.rag_service import rag_service
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Knowria AI")
 app.include_router(router)
+
+app.add_middleware(
+CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Load vectorstore on app start
 @app.on_event("startup")
